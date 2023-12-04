6048567736a4d464da7f59dea084e6ec5e32b1aa
Gad-Ongoro <gadongoro1@gmail.com>
2023-12-04 16:23:30 +0300

    EDITED

--- schl_mngnt.sql
+++ schl_mngnt.sql
@@ -10,9 +10,9 @@
 
 -- ~ C R U D
 
-CREATE DATABASE school_management_system;
+-- CREATE DATABASE school_management_system;
 
-USE DATABASE school_management_system;
+-- USE DATABASE school_management_system;
 
 SHOW TABLES;
 
@@ -20,8 +20,10 @@ SHOW TABLES;
 CREATE TABLE courses(
     course_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
+    sup_id INT,
     duration VARCHAR(50)
 );
+DESCRIBE courses;
 
 -- C sup --
 CREATE TABLE supervisors(
@@ -40,12 +42,12 @@ CREATE TABLE software_engineering_students(
     major VARCHAR(100),
     languages VARCHAR(100),
     status VARCHAR(100),
-    work_availability VARCHAR(100)
+    work_availability VARCHAR(100),
+    sup_id INT
 );
 
 -- add column --
-ALTER TABLE software_engineering_students
-ADD date_started DATE;
+ALTER TABLE software_engineering_students ADD date_started DATE;
 
 DESCRIBE software_engineering_students;
 
@@ -59,7 +61,8 @@ CREATE TABLE cybersecurity_students(
     languages VARCHAR(100),
     status VARCHAR(100),
     work_availability VARCHAR(100),
-    date_started DATE
+    date_started DATE,
+    sup_id INT
 );
 
 -- C UI/UX --
@@ -72,35 +75,38 @@ CREATE TABLE ui_ux_students(
     languages VARCHAR(100),
     status VARCHAR(100),
     work_availability VARCHAR(100),
-    date_started DATE
+    date_started DATE,
+    sup_id INT
 );
 
 SHOW TABLES;
 
 -- ALTER -- 
--- ALTER TABLE courses ADD FOREIGN KEY (sup_id) REFERENCES supervisors(sup_id) ON DELETE SET NULL;
--- ALTER TABLE cybersecurity_students ADD FOREIGN KEY (sup_id) REFERENCES supervisors (sup_id) ON DELETE SET NULL;
--- ALTER TABLE software_engineering_students ADD FOREIGN KEY (sup_id) REFERENCES supervisors (sup_id) ON DELETE SET NULL;
--- ALTER TABLE ui_ux_students ADD FOREIGN KEY (sup_id) REFERENCES supervisors (sup_id) ON DELETE SET NULL;
+ALTER TABLE courses ADD FOREIGN KEY (sup_id) REFERENCES supervisors(sup_id) ON DELETE SET NULL;
+ALTER TABLE cybersecurity_students ADD FOREIGN KEY (sup_id) REFERENCES supervisors (sup_id) ON DELETE SET NULL;
+ALTER TABLE software_engineering_students ADD FOREIGN KEY (sup_id) REFERENCES supervisors (sup_id) ON DELETE SET NULL;
+ALTER TABLE ui_ux_students ADD FOREIGN KEY (sup_id) REFERENCES supervisors (sup_id) ON DELETE SET NULL;
 
 
 -- INSERT (I)
 -- I sup --
 INSERT INTO supervisors
 VALUES
-(1, "Khalifa Muyideen", "kmuyideen@outlook.com", "FullStack"),
-(2, "Abdi Rashid", "abdulrashid@gmail.com", "FrontEnd")
+(1001, "Abdi Rashid", "abdulrashid@gmail.com", "FrontEnd"),
+(1002, "Sean Newton", "seannewton@gmail.com", "BackEnd"),
+(1003, "Khalifa Muyideen", "kmuyideen@outlook.com", "FullStack"),
 ;
 SELECT * FROM supervisors;
 
 -- I SE --
+DESCRIBE software_engineering_students;
 INSERT INTO software_engineering_students VALUES
-(1, "Nathan Kiprotich", "nathan.kiprotich@student.moringaschool.com", "G301", "FrontEnd", "REACT", "Active", "Available", "2023-09-04"),
-(2, "Beatrice Mwenje", "beatrice.mwenje@student.moringaschool.com", "G302", "FullStack", "PHP", "Active", "Available", "2023-09-04"),
-(3, "Kibet Nathan", "kibet.nathan@student.moringaschool.com", "G303", "BackEnd", "SQL", "Active", "Available", "2023-09-04"),
-(4, "Linder Opondo", "linder.opondo@student.moringaschool.com", "G304", "FullStack", "PYTHON", "Active", "Available", "2023-09-04"),
-(5, "Gad Ongoro", "gad.ongoro@student.moringaschool.com", "G305", "FullStack", "C", "Active", "Available", "2023-09-04"),
-(6, "Samuel Otieno", "samuel.otieno@student.moringaschool.com", "G306", "FrontEnd", "REACT", "Active", "Available", "2023-09-04")
+(1, "Nathan Kiprotich", "nathan.kiprotich@student.moringaschool.com", "G301", "FrontEnd", "REACT", "Active", "Available", 1001, "2023-09-04"),
+(2, "Beatrice Mwenje", "beatrice.mwenje@student.moringaschool.com", "G302", "FullStack", "PHP", "Active", "Available", 1003, "2023-09-04"),
+(3, "Kibet Nathan", "kibet.nathan@student.moringaschool.com", "G303", "BackEnd", "SQL", "Active", "Available", 1002, "2023-09-04"),
+(4, "Linder Opondo", "linder.opondo@student.moringaschool.com", "G304", "FullStack", "PYTHON", "Active", "Available", 1003, "2023-09-04"),
+(5, "Gad Ongoro", "gad.ongoro@student.moringaschool.com", "G305", "FullStack", "C", "Active", "Available", 1003, "2023-09-04"),
+(6, "Samuel Otieno", "samuel.otieno@student.moringaschool.com", "G306", "FrontEnd", "REACT", "Active", "Available", 1001, "2023-09-04")
 ;
 
 -- R SE --
@@ -116,21 +122,23 @@ UPDATE software_engineering_students
 SET major = "FE"
 WHERE major = "FrontEnd";
 
+SELECT * FROM software_engineering_students;
+
 -- D SE --
+DELETE FROM software_engineering_students WHERE id = 5;
+
 DELETE FROM software_engineering_students;
 
-DELETE FROM software_engineering_students
-WHERE id = 5
-;
+SELECT * FROM software_engineering_students;
 
 -- I CS --
 INSERT INTO cybersecurity_students VALUES
-(1, "Nathan Kiprotich", "nathan.kiprotich@student.moringaschool.com", "G301", "FrontEnd", "REACT", "Active", "Available", "2023-09-04"),
-(2, "Beatrice Mwenje", "beatrice.mwenje@student.moringaschool.com", "G302", "FullStack", "PHP", "Active", "Available", "2023-09-04"),
-(3, "Kibet Nathan", "kibet.nathan@student.moringaschool.com", "G303", "BackEnd", "SQL", "Active", "Available", "2023-09-04"),
-(4, "Linder Opondo", "linder.opondo@student.moringaschool.com", "G304", "FullStack", "PYTHON", "Active", "Available", "2023-09-04"),
-(5, "Gad Ongoro", "gad.ongoro@student.moringaschool.com", "G305", "FullStack", "C", "Active", "Available", "2023-09-04"),
-(6, "Samuel Otieno", "samuel.otieno@student.moringaschool.com", "G306", "FrontEnd", "REACT", "Active", "Available", "2023-09-04")
+(1, "Nathan Kiprotich", "nathan.kiprotich@student.moringaschool.com", "G301", "FrontEnd", "REACT", "Active", "Available", "2023-09-04", 1001),
+(2, "Beatrice Mwenje", "beatrice.mwenje@student.moringaschool.com", "G302", "FullStack", "PHP", "Active", "Available", "2023-09-04", 1003),
+(3, "Kibet Nathan", "kibet.nathan@student.moringaschool.com", "G303", "BackEnd", "SQL", "Active", "Available", "2023-09-04", 1002),
+(4, "Linder Opondo", "linder.opondo@student.moringaschool.com", "G304", "FullStack", "PYTHON", "Active", "Available", "2023-09-04", 1003),
+(5, "Gad Ongoro", "gad.ongoro@student.moringaschool.com", "G305", "FullStack", "C", "Active", "Available", "2023-09-04", 1003),
+(6, "Samuel Otieno", "samuel.otieno@student.moringaschool.com", "G306", "FrontEnd", "REACT", "Active", "Available", "2023-09-04", 1001)
 ;
 SELECT * FROM cybersecurity_students;
 
