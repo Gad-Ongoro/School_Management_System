b9d51a6fad100f67d9101e12859d6da577a02d09
Gad-Ongoro <gadongoro1@gmail.com>
2023-12-20 15:36:49 +0300

    Added queries

--- schl_mngnt.sql
+++ schl_mngnt.sql
@@ -3,12 +3,13 @@
 -- Phases (phases)
 -- Students (stud)
 -- Supervisor (sup)
--- ~ Software Engineering
--- ~ Data Science 
--- ~ Cybersecurity
--- ~ Product Designs
--- ~ DevOps Engineering
--- ~ Data Visualization with Python
+
+-- ~ Software Engineering (Phase-0)
+-- ~ Product Design (Phase-1)
+-- ~ Data Science (Phase-2)
+-- ~ Data Visualization with Python (Phase-3)
+-- ~ DevOps Engineering (Phase-4)
+-- ~ Cybersecurity (Phase-5)
 
 -- C R U D
 
@@ -16,7 +17,7 @@
 
 -- USE DATABASE school_management_system;
 
--- SHOW TABLES;
+SHOW TABLES;
 
 -- C courses --
 CREATE TABLE courses(
@@ -48,9 +49,9 @@ CREATE TABLE students(
     name VARCHAR(100),
     email VARCHAR(100),
     reg_id VARCHAR(100),
-    major VARCHAR(100),
+    course_id INT,
     status VARCHAR(100),
-    phase VARCHAR(20),
+    phase INT,
     work_availability VARCHAR(100),
     sup_id INT
 );
@@ -66,30 +67,43 @@ SHOW TABLES;
 -- ALTER -- 
 ALTER TABLE courses ADD FOREIGN KEY (sup_id) REFERENCES supervisors(sup_id) ON DELETE SET NULL;
 ALTER TABLE students ADD FOREIGN KEY (sup_id) REFERENCES supervisors (sup_id) ON DELETE SET NULL;
+ALTER TABLE students ADD FOREIGN KEY (phase) REFERENCES phases (phase_id) ON DELETE SET NULL;
 
 -- INSERT (I)
+-- I phases --
+DESCRIBE phases;
+INSERT INTO phases VALUES
+(1, "Phase-0", 1000),
+(2, "Phase-1", 1001),
+(3, "Phase-2", 1002),
+(4, "Phase-3", 1003),
+(5, "Phase-4", 1004),
+(6, "Phase-5", 1005)
+;
+
 -- I sup --
 INSERT INTO supervisors
 VALUES
+(1000, "Khalifa Muyideen", "kmuyideen@outlook.com", "FullStack"),
 (1001, "Abdi Rashid", "abdulrashid@gmail.com", "FrontEnd"),
 (1002, "Sean Newton", "seannewton@gmail.com", "BackEnd"),
-(1003, "Khalifa Muyideen", "kmuyideen@outlook.com", "FullStack"),
-(1004, "David Kingston", "davidkings@gmail.com", "Data Science"),
-(1005, "Emma Maart", "emmamaart@outlook.com", "DevOps Engineering")
+(1003, "David Kingston", "davidkings@gmail.com", "Data Science"),
+(1004, "Emma Maart", "emmamaart@outlook.com", "DevOps Engineering"),
+(1005, "Wales Wallace", "wallace1@yahoo.com", "BackEnd")
 ;
 SELECT * FROM supervisors;
 
 DELETE FROM supervisors WHERE sup_id = 1003;
 
--- I SE --
+-- I stud --
 DESCRIBE students;
 INSERT INTO students VALUES
-(1, "Nathan Kiprotich", "nathan.kiprotich@student.moringaschool.com", "G301", "FrontEnd", "Active", "Phase-3", "Available", 1001, "2023-09-04"),
-(2, "Beatrice Mwenje", "beatrice.mwenje@student.moringaschool.com", "G302", "FullStack",  "Active", "Phase-3", "Available", 1003, "2023-09-04"),
-(3, "Kibet Nathan", "kibet.nathan@student.moringaschool.com", "G303", "BackEnd", "Active", "Phase-3", "Available", 1002, "2023-09-04"),
-(4, "Linder Opondo", "linder.opondo@student.moringaschool.com", "G304", "FullStack", "Active", "Phase-3", "Available", 1003, "2023-09-04"),
-(5, "Gad Ongoro", "gad.ongoro@student.moringaschool.com", "G305", "FullStack", "Active", "Phase-3", "Available", 1003, "2023-09-04"),
-(6, "Samuel Otieno", "samuel.otieno@student.moringaschool.com", "G306", "FrontEnd", "Active", "Phase-3", "Available", 1001, "2023-09-04")
+(1, "Nathan Kiprotich", "nathan.kiprotich@student.moringaschool.com", "G301", 100, "Active", 3, "Available", 1001, "2023-09-04"),
+(2, "Beatrice Mwenje", "beatrice.mwenje@student.moringaschool.com", "G302", 100,  "Active", 3, "Available", 1003, "2023-09-04"),
+(3, "Kibet Nathan", "kibet.nathan@student.moringaschool.com", "G303", 100, "Active", 3, "Available", 1002, "2023-09-04"),
+(4, "Linder Opondo", "linder.opondo@student.moringaschool.com", "G304", 100, "Active", 3, "Available", 1003, "2023-09-04"),
+(5, "Gad Ongoro", "gad.ongoro@student.moringaschool.com", "G305", 100, "Active", 3, "Available", 1003, "2023-09-04"),
+(6, "Samuel Otieno", "samuel.otieno@student.moringaschool.com", "G306", 100, "Active", 3, "Available", 1001, "2023-09-04")
 ;
 
 -- R stud --
