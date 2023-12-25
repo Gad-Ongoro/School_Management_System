9257c4ca0617317ec9d576db7e551fc604037525
Gad-Ongoro <gadongoro1@gmail.com>
2023-12-25 13:45:18 +0300

    CRUD using OOP on Phases

--- courses_seed.py
+++ courses_seed.py
@@ -2,10 +2,11 @@ from sqlalchemy.orm import sessionmaker
 from models import engine, Course
 import click
 
+""" Using click CLI Library to get and map user input to the DataBase """
 @click.command()
-@click.option("--count", default=1)
-@click.option("--name", prompt="Name")
-@click.option("--sup_id", prompt="Supervisor ID")
+# @click.option("--count", default=1)
+# @click.option("--name", prompt="Name")
+# @click.option("--sup_id", prompt="Supervisor ID")
 
 def course_handler(count, name, sup_id):
     for _ in range(count):
@@ -16,7 +17,7 @@ def course_handler(count, name, sup_id):
         course = {"Name": name, "Supervisor ID": sup_id}
         print(course)
 
-        new_course = Course(name =  name, sup_id = sup_id)
+        new_course = Course(name = name, sup_id = sup_id)
 
         session.add(new_course)
         session.commit()
@@ -25,15 +26,63 @@ def course_handler(count, name, sup_id):
 if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
-    course_handler()
+    # course_handler()
     pass
 
-course_1 = Course(name = "Software Engineering", sup_id = 1)
-course_2 = Course(name = "Product Design", sup_id = 1)
-course_3 = Course(name = "Data Science", sup_id = 1)
-course_4 = Course(name = "Data Visualization with Python", sup_id = 1)
-course_5 = Course(name = "DevOps Engineering", sup_id = 1)
-course_6 = Course(name = "Cybersecurity", sup_id = 1)
+""" CREATE """
+# filling DB with initial set of data C
+course_1 = Course(name = "Software Engineering", sup_id = 1000)
+course_2 = Course(name = "Product Design", sup_id = 1001)
+course_3 = Course(name = "Data Science", sup_id = 1002)
+course_4 = Course(name = "Data Visualization with Python", sup_id = 1003)
+course_5 = Course(name = "DevOps Engineering", sup_id = 1004)
+course_6 = Course(name = "Cybersecurity", sup_id = 1005)
 
 # session.add_all([course_1, course_2, course_3, course_4, course_5, course_6])
 # session.commit()
+
+""" CRUD using OOP """
+class course_data_query:
+    def __init__(self):
+        pass
+    
+    # CREATE    
+    def add_new_course(self, name, sup_id):
+        new_course = Course(name = name, sup_id = sup_id)
+        session.add(new_course)
+        session.commit()
+    
+    # READ
+    def get_all_courses_data(self):
+        all_courses = session.query(Course).all() #.all() returns a list []
+        for course in all_courses:
+            print(course)     
+        # print(all_courses)
+    
+    # UPDATE
+    def update_course_data(self, id:int, new_sup):
+        for course in session.query(Course).all(): #using a list to update course
+            if course.course_id == id:
+                course.sup_id = new_sup
+        session.commit()
+    
+    # DELETE
+    def delete_course(self, id):
+        for course in session.query(Course).all():
+            if course.course_id == id:
+                session.delete(course)
+                session.commit()
+    
+course_data = course_data_query()
+
+""" CREATE """
+course_data.add_new_course("Mobile Application Development", 1004)
+
+""" READ """
+course_data.get_all_courses_data()
+
+""" UPDATE """
+course_data.update_course_data(4, 1010)
+
+""" DELETE """
+course_data.delete_course(7)
